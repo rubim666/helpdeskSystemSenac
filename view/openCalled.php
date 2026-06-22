365a8bb1d7c416524b463ba65a50c221a452d64d
nickzin-art <nicolasbez2009.11@gmail.com>
2026-06-22 11:38:39 -0300

    fazendo tela de Abrir Chamado

---
+++ view/openCalled.php
@@ -0,0 +1,29 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abrir Chamado</title>
+</head>
+<body>
+    <div class="form">
+        <form action="" method="post">
+            <div class="form-label">
+                <label for="HTML" id="called-id"><?php #ID DO CHAMADO ?></label>
+            </div>
+
+            <div class="form-label">
+                <input type="text" name="title" id="title">
+            </div>
+
+            <div class="form-label">
+                <input type="text" name="description" id="description">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
