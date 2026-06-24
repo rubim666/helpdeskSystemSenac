94a8f80bad8caca3c95ff3fafc39c8b2b169e703
Riq <ruylivinginadream@duck.com>
2026-06-24 09:12:16 -0300

    NO NODE_MODULES

--- chirper/resources/js/data/mock.ts
+++ chirper/resources/js/data/mock.ts
@@ -4,7 +4,7 @@ import type {
     HelpdeskStatus,
     HelpdeskTicket,
     HelpdeskUser,
-} from '@/types/helpdesk';
+} from '../types/helpdesk';
 
 export const currentUser: HelpdeskUser = {
     id: 1,
