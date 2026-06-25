697ef80532e9628b0a1abd019e432c1073f50d1a
Caiquef80 <caiquecampos.767@gmail.com>
2026-06-25 09:23:59 -0300

    fix - Apagando as pastas duplicadas

--- src/models/Analyst.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once  __DIR__ . "/User.php";
-class Analyst extends User{
-    public function __construct(int $id, string $uuid, string $nome, string $cpf, string $telefone, string $email, string $senha, string $nivel = 'analista', bool $ativo = true)
-    {
-        parent::__construct($id, $uuid, $nome, $cpf, $telefone, $email, $senha, $nivel, $ativo);
-    }
-}
-?>
